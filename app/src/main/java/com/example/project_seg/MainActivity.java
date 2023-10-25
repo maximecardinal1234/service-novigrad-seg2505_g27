8a6fa7ed27e2d2bc8371a6520670fb2162a1457b
maximecardinal1234 <mcard024@uottawa.ca>
2023-10-25 17:18:26 -0400

    Initial commit

---
+++ app/src/main/java/com/example/project_seg/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.project_seg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView username;
+    TextView password;
+    Button loginButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        loginButton = findViewById(R.id.loginButton);
+
+
+    }
+
+}
